ffec9eb2ca5dab2f36a9a17db3919222a384a7a1
J. Tangelder <j.tangelder@gmail.com>
2026-01-18 13:38:42 +0100

    more formatting

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    environment: 'jsdom',
+    environment: "jsdom",
     globals: true,
     coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
-      include: ['src/lib2/**/*.ts'],
-      exclude: ['src/lib2/**/*.test.ts', 'src/lib2/README.md', 'src/lib2/protocol.txt'],
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      include: ["src/lib2/**/*.ts"],
+      exclude: [
+        "src/lib2/**/*.test.ts",
+        "src/lib2/README.md",
+        "src/lib2/protocol.txt",
+      ],
     },
   },
 });
